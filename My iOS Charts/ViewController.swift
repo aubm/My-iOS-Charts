827c15d97e202a6e90395efccc2048275412e4cb
kendo5731 <aur.baumann@gmail.com>
2017-01-19 23:09:30 +0100

    Initial Commit

---
+++ My iOS Charts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My iOS Charts
+//
+//  Created by Aurélien Baumann on 19/01/2017.
+//  Copyright © 2017 Aurélien Baumann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
